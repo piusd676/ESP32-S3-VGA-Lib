0d0e9d3144fdf1921c9ad4b060d4d1ab2e2f9a78
piusd676 <piusdanner170@gmail.com>
2025-12-24 00:34:37 +0100

    Ajusted for saving parameters and flexible bits per pixel

--- main/VGADisplayManager.cpp
+++ main/VGADisplayManager.cpp
@@ -15,30 +15,33 @@ static uint8_t *pixels8 = NULL;
 static uint16_t *pixels = NULL;
 static void *pixelsv = NULL;
 int bitpp = 0;
-std::vector<std::vector<int>> frame;
+int width = 0;
+int height = 0;
 
-void VGADisplayManager::initGraphics(void *framebuffer, int width, int height, int pbitpp) {
+void VGADisplayManager::initGraphics(void *framebuffer, int pwidth, int pheight, int pbitpp) {
     pixels = (uint16_t *)framebuffer;
     pixels8 = (uint8_t *)framebuffer;
     pixelsv = framebuffer;
 
-    bitpp = pbitpp;
+    width = pwidth;
+    height = pheight;
 
-    frame.resize(height);
-    for(int i = 0; i < frame.size(); i++) {
-        frame[i].resize(width);
-    }
-    for(int y = 0; y < frame.size(); y++) {
-        for(int x = 0; x < frame[y].size(); x++) {
-            frame[y][x] = 0;
-        }
-    }
+    bitpp = pbitpp;
 }
 
 void VGADisplayManager::displayRectangle(int color, int px, int py, int rwidth, int rheight) {
-    for(int y = py; y < (py + rheight); y++) {
-        for(int x = px; x < (px + rwidth); x++) {
-            pixels8[y * 640 + x] = color;
+    if(bitpp == 8) {
+        for(int y = py; y < (py + rheight); y++) {
+            for(int x = px; x < (px + rwidth); x++) {
+                pixels8[y * 640 + x] = color;
+            }
+        }
+    }
+    if(bitpp == 16) {
+        for(int y = py; y < (py + rheight); y++) {
+            for(int x = px; x < (px + rwidth); x++) {
+                pixels[y * 640 + x] = color;
+            }
         }
     }
 }
